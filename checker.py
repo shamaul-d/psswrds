105290f5c0c2da86a9a297ec6ebd55896270787f
shamaul-d <sdilmohamed@stuy.edu>
2017-04-21 14:47:21 -0400

    done?

--- checker.py
+++ checker.py
@@ -18,4 +18,41 @@ def pstrength(s):
     num = [chr(a) for a in range(48,58)]
     cap = [chr(b) for b in range(97,123)]
     low = [chr(c) for c in range(65,91)]
-    return 1
+    misc = [chr(d) for d in range(33,48)]
+    misc2 = [chr(e) for e in range(58,65)]
+    ansi = [f for f in h if f in num]
+    ansc = [g for g in h if g in cap]
+    ansl = [i for i in h if i in low]
+    ansm = [j for j in h if i in misc or i in misc2]
+    i = 0
+    length = 0
+    if (len(h) > 7):
+        length = 2
+    if (len(ansi) != 0):
+        i = 2
+    c = 0
+    if (len(ansc) != 0):
+        c = 1
+    l = 0
+    if (len(ansl) != 0):
+        l = 1
+    lets = c + l
+    if (c == 1 and l == 1):
+        lets = 3
+    m = 0
+    if (len(ansm) != 0):
+        m = 4
+    ans = length + i + lets + m
+    if (ans > 10):
+        ans = 10
+    return ans
+
+
+print pstrength("asdfghjkl")
+print pstrength("asdfGHJKL")
+print pstrength("asdf4ik")
+print pstrength("asdfgQWERT789!*")
+print pstrength("a1L")
+print pstrength("a1L!!")
+
+
